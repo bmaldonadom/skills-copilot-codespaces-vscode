3cf58784137c85c692490209e97ce85b32e3a24a
bmaldonadom <bernarda.maldonado.m@gmail.com>
2024-08-05 04:23:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,120 @@
+function skillsMember() {
+  const skills = [
+    {
+      name: 'HTML',
+      rate: 5,
+    },
+    {
+      name: 'CSS',
+      rate: 5,
+    },
+    {
+      name: 'JavaScript',
+      rate: 5,
+    },
+    {
+      name: 'React',
+      rate: 5,
+    },
+    {
+      name: 'Node.js',
+      rate: 5,
+    },
+    {
+      name: 'Express',
+      rate: 5,
+    },
+    {
+      name: 'MongoDB',
+      rate: 5,
+    },
+    {
+      name: 'Python',
+      rate: 4,
+    },
+    {
+      name: 'Django',
+      rate: 4,
+    },
+    {
+      name: 'PostgreSQL',
+      rate: 4,
+    },
+    {
+      name: 'Git',
+      rate: 5,
+    },
+    {
+      name: 'Docker',
+      rate: 4,
+    },
+    {
+      name: 'Linux',
+      rate: 5,
+    },
+    {
+      name: 'AWS',
+      rate: 4,
+    },
+    {
+      name: 'Azure',
+      rate: 3,
+    },
+    {
+      name: 'GCP',
+      rate: 3,
+    },
+    {
+      name: 'Heroku',
+      rate: 4,
+    },
+    {
+      name: 'Netlify',
+      rate: 4,
+    },
+    {
+      name: 'Vercel',
+      rate: 4,
+    },
+    {
+      name: 'Nginx',
+      rate: 4,
+    },
+    {
+      name: 'Apache',
+      rate: 4,
+    },
+    {
+      name: 'Jenkins',
+      rate: 4,
+    },
+    {
+      name: 'Travis CI',
+      rate: 4,
+    },
+    {
+      name: 'Circle CI',
+      rate: 4,
+    },
+    {
+      name: 'GitHub Actions',
+      rate: 4,
+    },
+    {
+      name: 'Jest',
+      rate: 4,
+    },
+    {
+      name: 'Mocha',
+      rate: 4,
+    },
+    {
+      name: 'Chai',
+      rate: 4,
+    },
+    {
+      name: 'Cypress',
+      rate: 4,
+    },
+    {
+      name: 'Puppet
